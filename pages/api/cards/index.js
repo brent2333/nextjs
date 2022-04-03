c8c5b2e78882e127700815486a11c43e2e591c95
Brent Lawson <brent.lawson@fleetcor.com>
2022-04-03 08:49:13 -0400

    add ag grid

---
+++ pages/api/cards/index.js
@@ -0,0 +1,62 @@
+import nc from 'next-connect'
+import cards from '../../../data/cards'
+const UIStatus = {
+    ACTIVE: "active",
+    EXPIRED: "expired",
+    HOLD: "hold",
+    BLOCKED: "blocked",
+    PENDING: "pending",
+    OTHER: "other"
+  };
+const statusMap =  {
+    A: UIStatus.ACTIVE,
+    "A/R REASON": UIStatus.HOLD,
+    CLOSED: UIStatus.HOLD,
+    CONVERTED: UIStatus.HOLD,
+    CUST: UIStatus.BLOCKED,
+    DAMAGED: UIStatus.BLOCKED,
+    DESTROYED: UIStatus.HOLD,
+    DRAGNET: UIStatus.HOLD,
+    EXPIRED: UIStatus.HOLD,
+    FRAUD: UIStatus.BLOCKED,
+    INACTIVE: UIStatus.HOLD,
+    INACTIVITY: UIStatus.HOLD,
+    LOST: UIStatus.BLOCKED,
+    "LOST/STOLEN": UIStatus.HOLD,
+    OTHER: UIStatus.HOLD,
+    PENDING: UIStatus.PENDING,
+    Replace: UIStatus.BLOCKED,
+    REPLACED: UIStatus.BLOCKED,
+    SERVICING: UIStatus.HOLD,
+    SOLD: UIStatus.HOLD,
+    SPARE: UIStatus.HOLD,
+    STOLEN: UIStatus.HOLD,
+    "SUSP-AUTH": UIStatus.HOLD,
+    SUSPECT: UIStatus.BLOCKED,
+    T: UIStatus.HOLD,
+    TERMINATED: UIStatus.HOLD,
+    TRANSFER: UIStatus.HOLD,
+    TURNED_ON: UIStatus.ACTIVE,
+    TURNED_OFF: UIStatus.HOLD,
+    VEHICLE: UIStatus.HOLD,
+    "VEHICLE SOLD": UIStatus.HOLD,
+    "Wrong PIN": UIStatus.BLOCKED
+  };
+
+
+const tableCards = cards.map(card => {
+    card.Status = statusMap[card.Status]
+    card.Card = card.Title + ' ' + card.LastFour
+    card.Fleet = 'VILLAGE OF HOLMEN POLICE DEPT 608721'
+    card.VehicleID = card.Alias
+    card.Profile = card.ProfileName
+    card.LastUsed = card.LastUpdatedDate.split('T')[0]
+    card.CardType = card.cardType.title
+})
+console.log('tableCards', tableCards);
+const handler = nc()
+  .get((req, res) => {
+    res.json({data: cards})
+  })
+
+export default handler
--- pages/cards/index.js
+++ pages/cards/index.js
@@ -1,5 +1,32 @@
-export default () => {
+import { useEffect, useState, useRef } from 'react';
+import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+export default ({cards}) => {
     return (
+        <div>
         <h1>Manage Cards</h1>
+        <div className="ag-theme-alpine" style={{height: 600, width: 1200}}>
+           <AgGridReact
+               rowData={cards}>
+                <AgGridColumn field="Status"></AgGridColumn>
+                <AgGridColumn field="Card"></AgGridColumn>
+                <AgGridColumn field="Fleet"></AgGridColumn>
+                <AgGridColumn field="Profile"></AgGridColumn>
+                <AgGridColumn field="VehicleID"></AgGridColumn>
+                <AgGridColumn field="LastUsed"></AgGridColumn>
+           </AgGridReact>
+       </div>
+        </div>
     )
 }
+
+export async function getServerSideProps() {
+    const res = await fetch(`http://localhost:3000/api/cards`)
+    const {data} = await res.json()
+    console.log(data)
+    return {
+      props: {cards: data}
+    }
+  }
